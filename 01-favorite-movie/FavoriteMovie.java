47b948fd3a5342910fbf97dc27757e424ab18cbe
RaphielG <114940366+RaphielG@users.noreply.github.com>
2022-10-19 11:41:51 -0400

    Add files via upload

---
+++ 01-favorite-movie/FavoriteMovie.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class FavoriteMovie here.
+ *
+ * @ Raphiel
+ * @ 09/21/22
+ */
+public class FavoriteMovie
+{
+    public static void main(String[] args) {
+        System.out.print("I like the movie: The Dark Knight"); 
+        System.out.print("I like this movie as one of my favorite action movies");
+        System.out.print("Im not a huge fan of batman but this movie is one of the best batman movies");
+        System.out.print("I would say many people agree with me because it is critically acclaimed for its acting");
+    }
+}
